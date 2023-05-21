b692f1ff757d1c629b76e0976cdbe5393fb9c9b9
VIVEK <tailorvivekgec19@gmail.com>
2023-05-21 18:37:22 +0530

    json

--- lib/json_people/screen/person_view.dart
+++ lib/json_people/screen/person_view.dart
@@ -6,7 +6,6 @@ import '../provider/peopleprovider.dart';
 
 class Person_View extends StatefulWidget {
   const Person_View({Key? key}) : super(key: key);
-
   @override
   State<Person_View> createState() => _Person_ViewState();
 }
@@ -31,7 +30,7 @@ class _Person_ViewState extends State<Person_View> {
           centerTitle: true,
           title: Row(mainAxisSize: MainAxisSize.min,
             children: [
-              Text("${personT!.peoples[index].nameModel!.lastname} ",style:TextStyle(fontSize: 22,),),
+              Text("${personT!.peoples[index].nameModel!.lastname}  ".toUpperCase(),style:TextStyle(fontSize: 22,),),
               Text("${personT!.peoples[index].nameModel!.firstname}".toUpperCase(),style:TextStyle(fontSize: 23,),),
             ],
           ),
@@ -39,32 +38,45 @@ class _Person_ViewState extends State<Person_View> {
 
         body: Column(
           children: [
-            Text("data")
-            /*
-                        "address": {
-                        "geolocation": {
-                          "lat": "-37.3159",
-                          "long": "81.1496"
-                        },
-                        "city": "kilcoole",
-                        "street": "new road",
-                        "number": 7682,
-                        "zipcode": "12926-3874"
-                      },
-                      "id": 1,
-                      "email": "john@gmail.com",
-                      "username": "johnd",
-                      "password": "m38rmF$",
-                      "name": {
-                        "firstname": "john",
-                        "lastname": "doe"
-                      },
-                      "phone": "1-570-236-7033",
-                      "__v": 0
-            */
+            Text(":: ${personT!.peoples[index].nameModel!.firstname} ::",style: TextStyle(fontSize: 23,fontWeight: FontWeight.w600),),
+            Details(title: "User ID",data: "${personT!.peoples[index].id}"),
+            Details(title: "V Code",data: "${personT!.peoples[index].v}"),
+
+            Divider(color: Colors.black26,height: 15,thickness: 1,),
+
+            Text(":: Personal Information ::",style: TextStyle(fontSize: 23,fontWeight: FontWeight.w600),),
+            Details(title: "User Name",data: "${personT!.peoples[index].username}"),
+            Details(title: "Phone No",data: "${personT!.peoples[index].phone}"),
+            Details(title: "Email Id",data: "${personT!.peoples[index].email}"),
+            Details(title: "Password",data: "${personT!.peoples[index].password}"),
+
+            Divider(color: Colors.black26,height: 15,thickness: 1,),
+            Text(":: Address ::",style: TextStyle(fontSize: 23,fontWeight: FontWeight.w600),),
+
+            Details(title: "Street",data: "${personT!.peoples[index].addressModel!.number} / ${personT!.peoples[index].addressModel!.street}"),
+            Details(title: "City",data: "${personT!.peoples[index].addressModel!.city}"),
+            Details(title: "Pin-Code",data: "${personT!.peoples[index].addressModel!.zipcode}"),
+            Details(title: "Geographical Location",data: "${personT!.peoples[index].addressModel!.geoLocate!.lat} , ${personT!.peoples[index].addressModel!.geoLocate!.long}"),
+
+
+
+
           ],
         ),
 
+      ),
+    );
+  }
+  Widget Details({String? title, String? data})
+  {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8,horizontal: 15),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text("$title :",style: TextStyle(fontWeight: FontWeight.w300,fontSize: 15),),
+          Text("$data",style: TextStyle(fontSize: 14),)
+        ],
       ),
     );
   }
